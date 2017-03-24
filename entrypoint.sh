e372cdbc4229d54a2070ea3e53525a39585e0d4a
Emory Merryman <emory.merryman@deciphernow.com>
2017-03-24 20:18:58 +0000

    trying

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,22 +15,24 @@
 #    along with baker.  If not, see <http://www.gnu.org/licenses/>.
 
 cd ${HOME} &&
-    mkdir code &&
-    ssh-keygen -f ${HOME}/.ssh/id_rsa -P "" -C "${UUID}" &&
-	curl --data-urlencode "key=$(cat ${HOME}/.ssh/id_rsa.pub)" --data-urlencode "title=${UUID}" https://gitlab.363-283.io/api/v3/user/keys?private_token=${GITLAB_PRIVATE_TOKEN}  &&
-	cp /opt/docker/config ${HOME}/.ssh &&
-	chmod 0600 ${HOME}/.ssh &&
-    git -C code init &&
-    git -C code config user.name "${GIT_USER_NAME}" &&
-    git -C code config user.email "${GIT_USER_EMAIL}" &&
-    git -C code remote add upstream ssh://upstream/cte/object-drive-ui.git &&
-    git -C code config remote.upstream.pushurl "you really didn't want to do that" &&
-    git -C code remote add origin ssh://origin/${GITLAB_USERID}/object-drive-ui.git &&
-    cp /opt/docker/post-commit.sh code/.git/hooks/post-commit &&
-    chmod 0500 code/.git/hooks/post-commit &&
-    git -C code fetch upstream develop &&
-    git -C code checkout upstream/develop &&
-    cp /opt/docker/Dockerfile . &&
-    cp /opt/docker/run.sh . &&
-    cp /opt/docker/entrypoint.sh . &&
+    mkdir root &&
+    mkdir root/code &&
+#    ssh-keygen -f ${HOME}/.ssh/id_rsa -P "" -C "${UUID}" &&
+#	curl --data-urlencode "key=$(cat ${HOME}/.ssh/id_rsa.pub)" --data-urlencode "title=${UUID}" https://gitlab.363-283.io/api/v3/user/keys?private_token=${GITLAB_PRIVATE_TOKEN}  &&
+#	cp /opt/docker/config ${HOME}/.ssh &&
+#	chmod 0600 ${HOME}/.ssh &&
+    git -C root/code init &&
+    git -C root/code config user.name "${GIT_USER_NAME}" &&
+    git -C root/code config user.email "${GIT_USER_EMAIL}" &&
+    git -C root/code remote add upstream ssh://upstream/cte/object-drive-ui.git &&
+    git -C root/code config remote.upstream.pushurl "you really didn't want to do that" &&
+    git -C root/code remote add origin ssh://origin/${GITLAB_USERID}/object-drive-ui.git &&
+    cp /opt/docker/post-commit.sh root/code/.git/hooks/post-commit &&
+    chmod 0500 root/code/.git/hooks/post-commit &&
+    git -C root/code fetch upstream develop &&
+    git -C root/code checkout upstream/develop &&
+    cp -r ${HOME}/.ssh root &&
+    cp /opt/docker/nested/Dockerfile . &&
+    cp /opt/docker/nested/run.sh . &&
+    cp /opt/docker/nested/entrypoint.sh . &&
     docker build --tag ${IMAGE_TAG} .
